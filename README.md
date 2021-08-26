3d1acd37b2deb7c59644b37cf50edc6a97f98166
Mike <mike706574@gmail.com>
2021-08-26 18:56:24 -0500

    Create specific page for base64 encoding

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # image
 
 Some image stuff in Java 16.
+
+## Development
+
+To run the app:
+
+```
+scripts/run
+```
+
+To watch for changes:
+
+```
+scripts/watch
+```
--- src/main/java/image/web/Base64Controller.java
+++ src/main/java/image/web/Base64Controller.java
@@ -3,22 +3,23 @@ package image.web;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Base64;
 
 @Controller
-public class IndexController {
-    @GetMapping(path = "/", produces = MediaType.TEXT_HTML_VALUE)
+@RequestMapping("base64")
+public class Base64Controller {
+    private static final String VIEW = "base64";
+
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
     public String getIndex() {
-        return "index";
+        return VIEW;
     }
 
-    @PostMapping(path = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.TEXT_HTML_VALUE)
     public String uploadImage(Model model,
                               @RequestParam("image") MultipartFile image) throws IOException {
         byte[] imageBytes = image.getBytes();
@@ -27,6 +28,6 @@ public class IndexController {
 
         model.addAttribute("output", encodedImage);
 
-        return "index";
+        return VIEW;
     }
 }
